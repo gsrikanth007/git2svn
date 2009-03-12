09e4019a04abea02bc288242ee513d7b799ef35e
roug <roug@906c100d-7b07-0410-bda9-df73dee6a547>
2009-03-12 08:21:22 +0000

    Typo
    git-svn-id: https://svn.eionet.europa.eu/repositories/PHP@136 906c100d-7b07-0410-bda9-df73dee6a547

--- branches/PAMDB-with-EES-style/htdocs/fetch/fetch_pam_status.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$id_status = $pam_status_fetch["id_status"];
-	$status = $pam_status_fetch["status"];
-?>
--- branches/PAMDB-with-EES-style/htdocs/fetch/fetch_val_sector.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$id_sector = $val_sector_fetch["id_sector"];
-	$sector = $val_sector_fetch["sector"];
-?>
