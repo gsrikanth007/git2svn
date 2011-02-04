2081c8e229a66dd7ff398bd00a3dcbfaff7c8f36
simpkrob <simpkrob@906c100d-7b07-0410-bda9-df73dee6a547>
2011-02-04 16:00:10 +0000

    Updated CmsBaseUrl helper to not remove trailing '/' if string length is 1
    git-svn-id: https://svn.eionet.europa.eu/repositories/PHP@426 906c100d-7b07-0410-bda9-df73dee6a547

--- trunk/IRIS2/app/application/library/Zend/View/Helper/CmsBaseUrl.php
+++ trunk/IRIS2/app/application/library/Zend/View/Helper/CmsBaseUrl.php
@@ -27,7 +27,7 @@ class Zend_View_Helper_CmsBaseUrl extends Zend_View_Helper_Abstract {
     public function getCmsBaseUrl() {
         $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/application.ini', APPLICATION_ENV);
         $cmsBaseUrl = (isset($config->options->cms->baseUrl)) ? $config->options->cms->baseUrl : '';
-        $cmsBaseUrl = rtrim($cmsBaseUrl, '/');
+        $cmsBaseUrl = (1 < strlen($cmsBaseUrl)) ? rtrim($cmsBaseUrl, '/') : $cmsBaseUrl;
         return $cmsBaseUrl;
     }
 
